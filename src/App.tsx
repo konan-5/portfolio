3fbf8603aa4375668822393fb9d0471da12fabe4
konan-5 <ikedahiroshi517@gmail.com>
2024-02-07 23:40:56 +0900

    Add contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './layout/Header'
 import About from './pages/About'
+import Contact from './pages/Contact'
 import Experience from './pages/Experience'
 import Hero from './pages/Hero'
 import Projects from './pages/Projects'
@@ -16,6 +17,7 @@ function App() {
         <Projects />
         <Skills />
         <Experience />
+        <Contact/>
       </main>
     </>
   )
