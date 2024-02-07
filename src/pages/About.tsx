33a1deeada2ae6361b34360fcdc03b1a46d80652
konan-5 <ikedahiroshi517@gmail.com>
2024-02-07 18:19:44 +0900

    Add experience page constants

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Element } from 'react-scroll';
 const About: React.FC = () => {
     return (
-        <Element name={'about'} id='about' className='py-10 md:py-20'>
+        <Element name={'about'} id='about' className='pb-10 py-20 md:py-20'>
             <div className='container mx-auto'>
                 <h2 className='mb-10 text-center text-3xl md:text-4xl font-rale font-medium dark:text-white' id="about-title">About</h2>
                 <h3 className='max-w-[90%] md:max-w-[80%] mx-auto text-sm md:text-lg text-center dark:text-secondary' id="about-description">
